7e169ac7bdb019ba7198e18de6cbd32b858a6780
Alexander Filimonov <Filimonov.Alexander.S@yandex.ru>
2023-02-05 16:00:18 +0500

    feat: implement city select

--- plants/src/js/index.js
+++ plants/src/js/index.js
@@ -2,12 +2,22 @@ console.log(`Self-estimation of work:\n
 Score: 125 / 100\n
 - [x] The images' focus changes due to the active tags (50/50);\n
 - [x] All drop-down lists have been realized (50/50);\n
-- [x] Choose any city you want (25/25).`);
+- [x] Choose any city you want and call (25/25).`);
+
+function toContacts() {
+    document.location='#contacts';
+}
+
+function toService() {
+    document.location='#service';
+}
 
 function dropHamburgerMenu() {
-    document.getElementById("hamburger").classList.toggle("change-cross");
-    document.getElementById("navigation").classList.toggle("show-navigation");
-    document.getElementById("header-overlay").classList.toggle("header-overlay");
+    if (!(document.getElementById('contacts-overlay').matches('.contacts-overlay'))) {
+        document.getElementById("hamburger").classList.toggle("change-cross");
+        document.getElementById("navigation").classList.toggle("show-navigation");
+        document.getElementById("header-overlay").classList.toggle("header-overlay");
+    }   
 }
 
 window.onclick = function(event) {
@@ -149,6 +159,90 @@ pricesBlockButton.onclick = function(event) {
     }
 }
 
-function toContacts() {
-    document.location='#contacts';
+function openCity() {
+    document.getElementsByClassName('contacts-city')[0].classList.toggle("menu-active");
+    document.getElementsByClassName('contacts-city-select')[0].classList.toggle("menu-drop");
 }
+
+function openAddress() {
+    document.getElementsByClassName('contacts-office')[0].classList.add("address-active");
+    hideImage();
+}
+
+let tableOffice = document.getElementsByClassName('contacts-office-table')[0].children[0];
+
+function cityCanandaigua() {
+    document.getElementsByClassName('contacts-city')[0].children[0].innerText = "Canandaigua, NY";
+    document.getElementsByClassName('contacts-city')[0].style.backgroundColor = "#C1E698";
+    tableOffice.children[0].children[1].innerText = "Canandaigua, NY";
+    tableOffice.children[1].children[1].innerText = "+1 585 393 0001";
+    tableOffice.children[2].children[1].innerText = "151 Charlotte Street";
+    openAddress();
+    openCity();
+}
+
+function cityNewYork() {
+    document.getElementsByClassName('contacts-city')[0].children[0].innerText = "New York City";
+    document.getElementsByClassName('contacts-city')[0].style.backgroundColor = "#C1E698";
+    tableOffice.children[0].children[1].innerText = "New York City";
+    tableOffice.children[1].children[1].innerText = "+1 212 456 0002";
+    tableOffice.children[2].children[1].innerText = "9 East 91st Street";
+    openAddress();
+    openCity();
+}
+
+function cityYonkers() {
+    document.getElementsByClassName('contacts-city')[0].children[0].innerText = "Yonkers, NY";
+    document.getElementsByClassName('contacts-city')[0].style.backgroundColor = "#C1E698";
+    tableOffice.children[0].children[1].innerText = "Yonkers, NY";
+    tableOffice.children[1].children[1].innerText = "+1 914 678 0003";
+    tableOffice.children[2].children[1].innerText = "511 Warburton Ave";
+    openAddress();
+    openCity();
+}
+
+function citySherrill() {
+    document.getElementsByClassName('contacts-city')[0].children[0].innerText = "Sherrill, NY";
+    document.getElementsByClassName('contacts-city')[0].style.backgroundColor = "#C1E698";
+    tableOffice.children[0].children[1].innerText = "Sherrill, NY";
+    tableOffice.children[1].children[1].innerText = "+1 315 908 0004";
+    tableOffice.children[2].children[1].innerText = "14 WEST Noyes BLVD";
+    openAddress();
+    openCity();
+}
+
+function startCalling() {
+    document.getElementById("contacts-overlay").classList.toggle("contacts-overlay");
+    document.getElementsByClassName('contacts-office')[0].classList.toggle("calling");
+    if (document.getElementsByClassName('contacts-office')[0].matches(".calling")) {
+        document.getElementsByClassName('contacts-office')[0].children[1].innerText = 'Stop calling...';
+    } else {
+        document.getElementsByClassName('contacts-office')[0].children[1].innerText = 'Call us';
+    }
+}
+
+function hideImage () {
+    if (((window.innerWidth > 768) && (window.innerWidth <= 1024) || (window.innerWidth <= 380)) && 
+    (document.getElementsByClassName('contacts-office')[0].matches(".address-active")) 
+    ) {
+        document.getElementsByClassName('contacts-wrapper')[0].children[0].style.width = "0px";
+    } else {
+        document.getElementsByClassName('contacts-wrapper')[0].children[0].style.width = "";
+    }
+
+    if ((window.innerWidth <= 768) && (window.innerWidth > 380) && 
+    (document.getElementsByClassName('contacts-office')[0].matches(".address-active"))) {
+        document.getElementsByClassName('contacts-wrapper')[0].children[0].style.marginTop = "285px";
+        document.getElementsByClassName('contacts-content')[0].children[0].style.marginBottom = "61px";
+    } else {
+        document.getElementsByClassName('contacts-wrapper')[0].children[0].style.marginTop = "";
+        document.getElementsByClassName('contacts-content')[0].children[0].style.marginBottom = "";
+    }
+
+    if ((window.innerWidth <= 380)) {
+        document.getElementsByClassName('contacts-wrapper')[0].children[0].style.marginTop = "172px";
+        document.getElementsByClassName('contacts-content')[0].children[0].style.marginBottom = "41px";
+    }
+}
+
+window.addEventListener('resize', hideImage);
